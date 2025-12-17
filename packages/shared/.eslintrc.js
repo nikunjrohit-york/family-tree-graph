4c09fc3b801185fff7614c57da06c461b421f743
nikunjrohit-york <nikunj.rohit@york.ie>
2025-12-17 16:00:34 +0530

    fix: update package.json scripts and ESLint configurations

--- packages/shared/.eslintrc.js
+++ packages/shared/.eslintrc.js
@@ -3,11 +3,11 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: 'module',
+    project: './tsconfig.json',
   },
   plugins: ['@typescript-eslint'],
   extends: [
     'eslint:recommended',
-    '@typescript-eslint/recommended',
   ],
   root: true,
   env: {
@@ -16,9 +16,7 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js', 'dist/**'],
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': 'warn',
   },
 };
